237af0d43115a700dec0ad4e9d53d81fc8634c67
dexgeh <dexgeh@gmail.com>
2013-04-14 14:34:47 +0200

    colorize pacman output

--- ap.zsh
+++ ap.zsh
@@ -15,7 +15,7 @@ function ap() {
   local command=$1
   case $command in
     (update)
-      sudo pacman -Scc --noconfirm
+      sudo pacman -Scc --noconfirm --color=always
       sudo reflector \
         --sort rate \
         --age 6 \
@@ -26,11 +26,11 @@ function ap() {
       curl -s https://www.archlinux.org/feeds/news/ \
         | xmllint --xpath //item/title\ \|\ //item/pubDate /dev/stdin \
         | sed -r -e 's:<title>([^<]*?)</title><pubDate>([^<]*?)</pubDate>:\2\t\1\n:g'
-      sudo pacman -Syu
+      sudo pacman -Syu --color=always
       cower -v -u
       ;;
     (install)
-      sudo pacman -S ${*:2}
+      sudo pacman -S --color=always ${*:2}
       ;;
     (compinst)
       mkdir -p ~/pkg
@@ -44,15 +44,15 @@ function ap() {
       popd
       ;;
     (remove)
-      sudo pacman -Rcsn ${*:2}
+      sudo pacman -Rcsn --color=always ${*:2}
       ;;
     (search)
-      pacman -Ss ${*:2}
+      pacman -Ss --color=always ${*:2}
       cower -s ${*:2}
       ;;
     (info)
       package=$2
-      pacman -Si $package
+      pacman -Si --color=always $package
       if [[ $? = 1 ]] then
         cower -i $package
       fi
